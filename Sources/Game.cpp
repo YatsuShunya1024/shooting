ffd52bd89d9aee272b42ca1c9696c713117bcc83
hw16a207 <hw16a207@oecu.jp>
2017-11-14 16:40:31 +0900

    雲の位置を動かす動作とBGMを追加

--- Sources/Game.cpp
+++ Sources/Game.cpp
@@ -1,14 +1,14 @@
 #include "Game.hpp"
 
 
-// TODO: 砲台の位置を画面左に、ターゲットの位置を画面右に移動させる。(A)
-// TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)
-// TODO: 砲台を青い壁に沿って上下に動かす。(C)
-// TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)
-// TODO: スコアのサイズを大きくする。(E)
-// TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)
-// TODO: PlayBGM()関数を使って、BGMを再生する。(G)
-// TODO: PlaySound()関数を使って、弾の発射時とターゲットに当たった時にSEを再生する。(H)
+// TODO: 砲台の位置を画面左に、ターゲットの位置を画面右に移動させる。(A)//黒津
+// TODO: 雲の位置を左から右に動かす。見えなくなったら左端に戻す。(B)//森本
+// TODO: 砲台を青い壁に沿って上下に動かす。(C)//谷津
+// TODO: 弾のスピードを速くし、弾が画面右端を通り越したら再度発射可能にする。(D)//新甚
+// TODO: スコアのサイズを大きくする。(E)//谷津
+// TODO: スコアを100点ずつ加算するようにし、5桁の表示に変える。(F)//新甚
+// TODO: PlayBGM()関数を使って、BGMを再生する。(G)//森本
+// TODO: PlaySound()関数を使って、弾の発射時とターゲットに当たった時にSEを再生する。(H)//黒津
 
 
 Vector2 cloudPos;       //!< 雲の位置
@@ -16,7 +16,6 @@ Vector2 cannonPos;      //!< 砲台の位置
 Vector2 bulletPos;      //!< 弾の位置
 Rect    targetRect;     //!< ターゲットの矩形
 int     score;          //!< スコア
-
 
 // ゲーム開始時に呼ばれる関数です。
 void Start()
@@ -26,6 +25,9 @@ void Start()
     targetRect = Rect(80, -140, 40, 40);
     bulletPos.x = -999;
     score = 0;
+    
+    //PlayBGM()関数でBGMを再生する(実装：HW16A207 森本義基)
+    PlayBGM("bgm_maoudamashii_8bit07.mp3");
 }
 
 // 1/60秒ごとに呼ばれる関数です。モデルの更新と画面の描画を行います。
@@ -47,7 +49,14 @@ void Update()
             bulletPos.x = -999; // 弾を発射可能な状態に戻す
         }
     }
-
+    //雲の位置を左から右に動かす。見えなくなったら左端に戻す(実装：HW16A207 森本義基)
+    if (cloudPos.x > -550) {
+        cloudPos.x += 60 * Time::deltaTime;
+        if (cloudPos.x > 340) {
+            cloudPos.x = -500;
+        }
+    }
+    
     // 背景の描画
     Clear(Color::cyan);
     FillRect(Rect(-320, -240, 640, 100), Color::green);
